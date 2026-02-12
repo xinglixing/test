a8dfe34e4177dde93939ff657b9027b2bb080691
xinglixing <xinglixing@users.noreply.github.com>
2026-02-12 00:14:56 -0800

    feat: add submodule and update main.go

--- main.go
+++ main.go
@@ -3,10 +3,10 @@ package main
 import "fmt"
 
 func add(a, b int) int {
-    return a + b
+	return a + b
 }
 
 func main() {
-    result := add(5, 3)
-    fmt.Println("Result of add(5, 3):", result)
+	result := add(5, 3)
+	fmt.Println("Result of add(5, 3):", result)
 }
